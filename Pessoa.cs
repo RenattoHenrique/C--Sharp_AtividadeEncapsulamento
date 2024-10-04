b0d194af8893b517f857c82d9001d008a2c143ba
RenattoHenrique <renato.trabalho17@gmail.com>
2024-10-04 19:37:42 -0300

    introdução de componentes
    Aplicação dos componentes

---
+++ Pessoa.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Encapsulamento
+{
+    public class Pessoa
+    {
+        private string nome;
+        private int idade;
+        private string telefone;
+        private string endereco;
+        private string email;
+
+        public string Nome
+        {
+            get { return nome; }
+            set{ 
+                if
+                (string.IsNullOrWhiteSpace(value))
+                    { Console.WriteLine("Nome não pode ser vazio."); }
+                else 
+                    { nome = value; }
+            }
+        }
+
+        public int Idade
+        {
+            get { return idade; }
+            set { 
+                if
+                (value < 0) 
+                    { Console.WriteLine("Idade não pode ser negativa."); }
+                else 
+                { idade = value; }
+            }
+        }
+
+        public string Telefone
+        {
+            get { return telefone; }
+            set { telefone = value; }
+        }
+
+        public string Endereco
+        {
+            get { return endereco; }
+            set { endereco = value; }
+        }
+
+        public string Email
+        {
+            get { return email; }
+            set { if (string.IsNullOrWhiteSpace(value)) { Console.WriteLine("E-mail não pode ser vazio.\n"); }
+                else { email = value; }
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Encapsulamento
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa pessoa = new Pessoa();
+
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.WriteLine(" ------------------ENCAPSULAMENTOS------------------ \n");
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.Gray;
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            pessoa.Nome = "";
+            pessoa.Idade = -100;
+            pessoa.Email = "";
+            Console.ForegroundColor = ConsoleColor.Gray;
+
+            
+            pessoa.Nome = "Renato Henrique de Oliveira Ramos";
+            pessoa.Idade = 27;
+            pessoa.Telefone = "(81) 98594-2139";
+            pessoa.Endereco = "Antiga PE 28, s/n - Jardim Santo Inacio, PE, 54500-000";
+            pessoa.Email = "renatohenrique@gmail.com";
+            
+
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.WriteLine("Nome: " + pessoa.Nome);
+            Console.WriteLine("Idade: " + pessoa.Idade + " Anos");
+            Console.WriteLine("Telefone: " + pessoa.Telefone);
+            Console.WriteLine("Endereço: " + pessoa.Endereco);
+            Console.WriteLine("E-mail: " + pessoa.Email);
+            Console.ForegroundColor = ConsoleColor.Gray;
+        }
+    }
+}
